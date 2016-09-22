7fb13f9ca84ab50ee59c1fbffb0aaa3349518d90
zider <qiufeng.amy@gmail.com>
2016-09-22 10:16:15 +0800

    add error apis

--- www/apis.py
+++ www/apis.py
@@ -7,4 +7,19 @@ import functools
 
 class APIError(Exception):
     def __init__(self, error, data='', message=''):
+        super(APIError, self).__init__(message)
+        self.error = error
+        self.data = data
+        self.message = message
         
+class APIValueError(APIError):
+    def __init__(self, field, message=''):
+        super(APIValueError, self).__init__('value:invalid', field, message)
+        
+class APIResourceNotFoundError(APIError):
+    def __init__(self, field, message=''):
+        super(APIResourceNotFoundError, self).__init__('value:not found', field, message)
+        
+class APIPermissionError(APIError):
+    def __init__(self, message=''):
+        super(APIPermissionError, self).__init__('value:forbidden', 'permission', message)
